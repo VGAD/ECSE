706acd265cee11f8d9331360d16e8c1234bceab0
Elliot Colp <thelastbanana@gmail.com>
2014-12-15 02:12:36 -0700

    Added a cleanup stage to make SFML stop crashing on exit.

--- Magnaut/Engine.cpp
+++ Magnaut/Engine.cpp
@@ -1,38 +1,49 @@
 #include "Engine.h"
+#include "Spritemap.h"
 #include <memory>
 
 namespace Engine
 {
-
-//! Private data.
-namespace
-{
-}
-
-ResourceManager<sf::Texture> textureManager;
-ResourceManager<sf::SoundBuffer> soundManager;
-
-void run()
-{
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Magnaut");
-
-    // Run the main loop
-    while (window.isOpen())
+    //! Private data.
+    namespace
     {
-        sf::Event event;
-
-        while (window.pollEvent(event))
+        //! Cleans up Engine data before closing.
+        void cleanup()
         {
-            if (event.type == sf::Event::Closed ||
-                (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape))
+            // These need to be called explicitly to avoid SFML getting angry about destroying
+            // resources without a context
+            textureManager.clear();
+            soundManager.clear();
+        }
+    }
+
+    ResourceManager<sf::Texture> textureManager;
+    ResourceManager<sf::SoundBuffer> soundManager;
+
+    void run()
+    {
+        sf::RenderWindow window(sf::VideoMode(800, 600), "Magnaut");
+
+        // Run the main loop
+        while (window.isOpen())
+        {
+            sf::Event event;
+
+            while (window.pollEvent(event))
             {
-                window.close();
+                if (event.type == sf::Event::Closed ||
+                    (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape))
+                {
+                    window.close();
+                }
             }
+
+            window.clear();
+            window.display();
         }
 
-        window.clear();
-        window.display();
+        // Clean everything up
+        cleanup();
     }
-}
 
 }
