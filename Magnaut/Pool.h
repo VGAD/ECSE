01eec1801a849a9ffd7bcdcae6e040716dc5715d
Elliot Colp <thelastbanana@gmail.com>
2014-12-22 18:30:53 -0700

    Added some usage information to PoolBase.

--- Magnaut/Pool.h
+++ Magnaut/Pool.h
@@ -1,12 +1,19 @@
 #pragma once
 
 //! Base class for object pools. Useful for maintaining containers of pointers to various Pool types.
+/*!
+  This class should probably only be used if you want to have a bunch of different pools of different
+  types and need a base class pointer. Otherwise, you're just wasting time and space on this.
+*/
 struct PoolBase
 {
     virtual ~PoolBase() { }
 };
 
-//! Specialized collection for each class type
+//! Specialized collection for each class type.
+/*!
+  \see PoolBase
+*/
 template <typename T>
 struct Pool : PoolBase
 {
