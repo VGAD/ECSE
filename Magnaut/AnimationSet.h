1665e4d6f14ae880ab4a9e9caf0b24d61a8defb6
Elliot Colp <thelastbanana@gmail.com>
2014-12-23 23:09:47 -0700

    Fixed/added some documentation.

--- Magnaut/AnimationSet.h
+++ Magnaut/AnimationSet.h
@@ -10,9 +10,9 @@ struct Animation
     //! Type for vector of animation frames indices.
     typedef std::vector<size_t> FrameVector;
 
-    FrameVector frames;
-    sf::Time frameTime;
-    bool looping;
+    FrameVector frames; //!< Set of frame indices used for this animation.
+    sf::Time frameTime; //!< The number of seconds per frame.
+    bool looping;       //!< Whether this animation is looping.
 };
 
 //! Contains information about a Spritemap animation.
@@ -21,11 +21,14 @@ class AnimationSet
 public:
     //! Load the animation data from a JSON file.
     /*!
-      \param fileName The name of the file from which to load the animation.
+      \param filename The name of the file from which to load the animation.
     */
     bool loadFromFile(const std::string& filename);
 
     //! Get an animation from this set.
+    /*!
+      \param name The name of the animation to get.
+    */
     const Animation& getAnimation(const std::string& name) const;
 
     // Data
