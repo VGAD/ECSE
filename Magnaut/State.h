b5d76fa30bcd690aae1772907532ba20a9e58230
Elliot Colp <thelastbanana@gmail.com>
2014-12-27 01:52:02 -0700

    Added a game State class.

---
+++ Magnaut/State.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <SFML/System.hpp>
+
+//! Handles a single game state.
+class State
+{
+    friend class Engine;
+
+public:
+    //! Destroy the State.
+    virtual ~State() {};
+
+    //! Called when this becomes active.
+    virtual void activate() = 0;
+
+    //! Called when this becomes inactive (not popped, but not being updated).
+    virtual void deactivate() = 0;
+
+    //! Perform the update step.
+    /*!
+    * \param deltaTime The time elapsed in this update.
+    */
+    virtual void update(sf::Time deltaTime) = 0;
+
+    //! Perform the advance step.
+    virtual void advance() = 0;
+
+    //! Perform the render step.
+    /*!
+    * \param alpha The amount of interpolation in this render.
+    */
+    virtual void render(double alpha) = 0;
+
+    //! Get the name of this State class.
+    /*!
+    * \return The name of the class.
+    */
+    virtual const std::string getName() = 0;
+
+protected:
+    //! The Engine to which this belongs.
+    Engine* engine = nullptr;
+};
+
