5776766e94ee94d309a1e37da074aa4a7af9270a
Elliot Colp <thelastbanana@gmail.com>
2014-12-31 17:48:59 -0700

    Fixed how float modulus is handled and added static asserts to make sure floats are used.

--- Magnaut/Common.h
+++ Magnaut/Common.h
@@ -36,10 +36,58 @@ inline float degToRad(float angle)
     return angle / 180.f * pi;
 }
 
+//! Performs modulus on a floating-point number, keeping it within the range of [0..y) or (y..0]
+/*!
+* Shamelessly stolen from: http://stackoverflow.com/a/4635752/858878
+*
+* \param x The dividend.
+* \param y The divisor.
+* \return (x % y) within the range [0..y], or (y..0] if y <= 0.
+*/
+template<typename T>
+T intervalMod(T x, T y)
+{
+    static_assert(!std::numeric_limits<T>::is_exact, "Floating-point type expected");
+
+    if (0. == y)
+        return x;
+
+    double m = x - y * floor(x/y);
+
+    // handle boundary cases resulted from floating-point cut off:
+
+    if (y > 0)              // modulo range: [0..y)
+    {
+        if (m>=y)           // Mod(-1e-16             , 360.    ): m= 360.
+            return 0;
+
+        if (m<0)
+        {
+            if (y+m == y)
+                return 0; // just in case...
+            else
+                return y+m; // Mod(106.81415022205296 , _TWO_PI ): m= -1.421e-14 
+        }
+    }
+    else                    // modulo range: (y..0]
+    {
+        if (m<=y)           // Mod(1e-16              , -360.   ): m= -360.
+            return 0;
+
+        if (m>0)
+        {
+            if (y+m == y)
+                return 0; // just in case...
+            else
+                return y+m; // Mod(-106.81415022205296, -_TWO_PI): m= 1.421e-14 
+        }
+    }
+
+    return m;
+}
+
 //! Smoothly interpolate between two values.
 /*!
-* You should probably only use floating-point types for this function.
-*
 * \param from The first value.
 * \param to The second value.
 * \param amount The interpolation amount.
@@ -48,6 +96,7 @@ inline float degToRad(float angle)
 template <typename T>
 inline T lerp(T from, T to, float amount)
 {
+    static_assert(!std::numeric_limits<T>::is_exact, "Floating-point type expected");
     return from + (to - from) * amount;
 }
 
@@ -76,8 +125,10 @@ inline T clamp(T min, T max, T value)
 template <typename T>
 T wrapDifference(T from, T to, T wrap)
 {
-    from = from % wrap;
-    to = to % wrap;
+    static_assert(!std::numeric_limits<T>::is_exact, "Floating-point type expected");
+
+    from = fmod(from, wrap);
+    to = fmod(to, wrap);
 
     T diffA = (to - from);
     T diffB = (abs(diffA) - wrap) * (diffA > T(0) ? T(-1) : T(1));
@@ -87,8 +138,6 @@ T wrapDifference(T from, T to, T wrap)
 
 //! Linearly interpolate between two values, wrapping over a given point.
 /*!
-* You should probably only use floating-point types for this function.
-*
 * \param from The first value.
 * \param to The second value.
 * \param wrap The wrapping point.
@@ -98,14 +147,14 @@ T wrapDifference(T from, T to, T wrap)
 template <typename T>
 inline T wrapLerp(T from, T to, T wrap, float amount)
 {
+    static_assert(!std::numeric_limits<T>::is_exact, "Floating-point type expected");
+
     T diff = wrapDifference(from, to, wrap);
     return lerp(from, from + diff, amount);
 }
 
 //! Linearly interpolate an angle taking into account periodicity.
 /*!
-* You should probably only use floating-point types for this function.
-*
 * \param from The first angle.
 * \param to The second angle.
 * \param amount The interpolation amount.
@@ -114,5 +163,7 @@ inline T wrapLerp(T from, T to, T wrap, float amount)
 template <typename T>
 inline T angularLerp(T from, T to, float amount)
 {
+    static_assert(!std::numeric_limits<T>::is_exact, "Floating-point type expected");
+
     return wrapLerp(from, to, T(pi), amount);
 }
