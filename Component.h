4a2f28c62ce2799880a43d453b8e83581e178ffd
Elliot Colp <thelastbanana@gmail.com>
2015-02-14 20:04:15 -0700

    Added a hook for when Components are attached.

--- Component.h
+++ Component.h
@@ -9,10 +9,20 @@
 class Component
 {
 public:
+    friend class Entity;
+
     //! Whether Systems should update this.
     /*!
     * Effectively, the System should behave as if this was not attached to its Entity.
     */
     bool enabled = true;
+
+protected:
+    //! Called when this is attached to an Entity.
+    /*!
+    * This is a good place to check requirements and/or notify other Components.
+    * \param e The Entity to which this was attached.
+    */
+    virtual void attached(Entity* e) {};
 };
 
