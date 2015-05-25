693bf56d5373899b300df3ad0c75afcbfa15cf73
Elliot Colp <thelastbanana@gmail.com>
2015-05-25 15:02:00 -0600

    Moved custom ASSERT macros to TestUtils.h

--- ECSETest/TestUtils.h
+++ ECSETest/TestUtils.h
@@ -1,5 +1,14 @@
+//! Check if an STL container contains something.
 template <typename ContainerType, typename ItemType>
 bool contains(ContainerType& container, ItemType item)
 {
     return std::find(container.begin(), container.end(), item) != container.end();
 }
+
+//! Use this to check angles taking wrapping into account.
+#define ASSERT_ANGLE_EQ(val1, val2) \
+    ASSERT_NEAR(ECSE::wrapDifference(val1, val2, 2.f * ECSE::pi), 0.f, ECSE::degToRad(0.01f))
+
+//! Use this when results come back from transcendental functions to make up for lost accuracy.
+#define ASSERT_NEAR_TRANS(val1, val2) \
+    ASSERT_NEAR(val1, val2, 0.0001f)
--- ECSETest/TestVectorMath.cpp
+++ ECSETest/TestVectorMath.cpp
@@ -1,14 +1,7 @@
 #include "gtest/gtest.h"
 #include "ECSE/VectorMath.h"
 #include "ECSE/Common.h"
-
-//! Use this to check angles taking wrapping into account.
-#define ASSERT_ANGLE_EQ(val1, val2) \
-    ASSERT_NEAR(ECSE::wrapDifference(val1, val2, 2.f * ECSE::pi), 0.f, ECSE::degToRad(0.01f))
-
-//! Use this when results come back from transcendental functions to make up for lost accuracy.
-#define ASSERT_NEAR_TRANS(val1, val2) \
-    ASSERT_NEAR(val1, val2, 0.0001f)
+#include "TestUtils.h"
 
 TEST(Rotate90Test, RightTest)
 {
