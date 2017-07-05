4324953547f0505f428ce7347c09ac61113e30f0
Elliot Colp <thelastbanana@gmail.com>
2017-07-04 20:16:39 -0600

    Fleshed out random functions more

---
+++ ECSE/Random.cpp
@@ -0,0 +1,27 @@
+#include "Random.h"
+#include "VectorMath.h"
+
+namespace ECSE
+{
+
+std::random_device randomDevice;
+std::mt19937 ECSE::randomEngine(randomDevice());
+
+sf::Vector2f ECSE::randomVector(float midAngle, float angleSpread, float minMag, float maxMag)
+{
+    auto angleDist = std::uniform_real_distribution<float>(
+        midAngle - angleSpread * 0.5f,
+        midAngle + angleSpread * 0.5f
+        );
+    auto magDist = std::uniform_real_distribution<float>(minMag, maxMag);
+
+    float angle = angleDist(randomEngine);
+    float mag = magDist(randomEngine);
+
+    auto v = sf::Vector2f(mag, 0.f);
+    setHeading(v, angle);
+
+    return v;
+}
+
+}
---
+++ ECSE/Random.h
@@ -0,0 +1,52 @@
+//! \file Random.h Contains functions for randomly generating numbers.
+/*!
+* If only functions/engines from this file are used, then determinism is guaranteed,
+* so it's highly recommended not to roll your own random functions.
+*/
+
+#pragma once
+
+#include <random>
+#include <SFML/System.hpp>
+
+namespace ECSE
+{
+
+//! The random engine that should be used for generating any random numbers in ECSE.
+extern std::mt19937 randomEngine;
+
+//! Generate a random vector.
+/*!
+* \param midAngle The midpoint of the possible angles.
+* \param angleSpread The spread of the possible angles (in radians).
+* \param minMag The minimum magnitude.
+* \param maxMag The maximum magnitude.
+* \return A reference v1.
+*/
+sf::Vector2f randomVector(float midAngle, float angleSpread, float minMag, float maxMag);
+
+//! Generate a random integer.
+/*!
+* \param min The minimum value.
+* \param max The maximum value.
+* \return An int in the range [min, max].
+*/
+inline int randomInt(int min, int max)
+{
+    auto dist = std::uniform_int_distribution<>(min, max);
+    return dist(randomEngine);
+}
+
+//! Generate a random float.
+/*!
+* \param min The minimum value.
+* \param max The maximum value.
+* \return A real number in the range [min, max].
+*/
+inline float randomFloat(float min, float max)
+{
+    auto dist = std::uniform_real_distribution<float>(min, max);
+    return dist(randomEngine);
+}
+
+}
